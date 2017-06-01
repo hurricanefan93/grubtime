ee4c8d7ba588edee1fa395ffb23bd701261338e4
Garret Morales <hurricanefan93@gmail.com>
2017-06-01 01:43:00 -0400

    clean up

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,10 +5,11 @@ import nightbbq from '../images/nightbbq.webm'
 class Landing extends Component {
   render () {
     return <div className='Landing'>
+      <button> Get Grub </button>
+      {/* <img src={logowhite} height='100px' width='100px' /> */}
       <video playsInline autoPlay muted loop>
         <source src={nightbbq} type='video/webm' />
       </video>
-      <img src={logowhite} height='100px' width='100px' />
     </div>
   }
 }
